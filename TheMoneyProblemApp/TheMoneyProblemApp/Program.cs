ae62d85b50a62b791961e7034419749bd4e3e894
Glen Smith <gsmith@spartaglobal.com>
2023-03-22 12:50:23 +0000

    Added REGEX method

--- TheMoneyProblemApp/TheMoneyProblemApp/Program.cs
+++ TheMoneyProblemApp/TheMoneyProblemApp/Program.cs
@@ -1,4 +1,4 @@
-﻿
+﻿using System.Text.RegularExpressions;
 namespace TheMoneyProblemApp
 {
     public class Program
@@ -11,7 +11,10 @@ namespace TheMoneyProblemApp
 
         public static string ReturnDenominations(string totalMoney)
         {
-            //throw new NotImplementedException();
+            //MULTIPLY BY 100
+            //REGEX CHECK
+            //TRY PARSE STRING TO DOUBLE
+
             double amountOfMoney = 0;
             int totalFivePound = 0;
             int totalTwoPound = 0;
@@ -23,7 +26,7 @@ namespace TheMoneyProblemApp
             int totalTwoPence = 0;
             int totalOnePence = 0;
 
-            if (amountOfMoney / 5 > 1)
+            if (amountOfMoney / 5 >= 1)
             {
                 totalFivePound = (int)amountOfMoney / 5;
                 amountOfMoney = amountOfMoney % 5;
@@ -38,39 +41,57 @@ namespace TheMoneyProblemApp
                 totalOnePound = (int)amountOfMoney / 1;
                 amountOfMoney = amountOfMoney % 1;
             }
-            // TO BE COMPLETED
-            if (amountOfMoney / 0.5 > 1)
-            {
-                totalFiftyPence = (int)amountOfMoney / 2;
-                amountOfMoney = amountOfMoney % 2;
-            }
-            if (amountOfMoney / 0.2 > 1)
-            {
-                totalTwentyPence = (int)amountOfMoney / 2;
-                amountOfMoney = amountOfMoney % 2;
-            }
-            if (amountOfMoney / 0.1 > 1)
-            {
-                totalTenPence = (int)amountOfMoney / 2;
-                amountOfMoney = amountOfMoney % 2;
-            }
-            if (amountOfMoney / 0.05 > 1)
-            {
-                totalFivePence = (int)amountOfMoney / 2;
-                amountOfMoney = amountOfMoney % 2;
-            }
-            if (amountOfMoney / 0.02 > 1)
-            {
-                totalTwoPence = (int)amountOfMoney / 2;
-                amountOfMoney = amountOfMoney % 2;
-            }
-            if (amountOfMoney / 0.0 > 1)
-            {
-                totalOnePence = (int)amountOfMoney / 2;
-                amountOfMoney = amountOfMoney % 2;
-            }
-            return "";
+            //// TO BE COMPLETED
+            //if (amountOfMoney / 0.5 > 1)
+            //{
+            //    totalFiftyPence = (int)amountOfMoney / 0.5;
+            //    amountOfMoney = amountOfMoney % 0.5;
+            //}
+            //if (amountOfMoney / 0.2 > 1)
+            //{
+            //    totalTwentyPence = (int)amountOfMoney / 0.2;
+            //    amountOfMoney = amountOfMoney % 0.2;
+            //}
+            //if (amountOfMoney / 0.1 > 1)
+            //{
+            //    totalTenPence = (int)amountOfMoney / 0.1;
+            //    amountOfMoney = amountOfMoney % 0.1;
+            //}
+            //if (amountOfMoney / 0.05 > 1)
+            //{
+            //    totalFivePence = (int)amountOfMoney / 0.05;
+            //    amountOfMoney = amountOfMoney % 0.05;
+            //}
+            //if (amountOfMoney / 0.02 > 1)
+            //{
+            //    totalTwoPence = (int)amountOfMoney / 0.02;
+            //    amountOfMoney = amountOfMoney % 0.02;
+            //}
+            //if (amountOfMoney / 0.01 > 1)
+            //{
+            //    totalOnePence = (int)amountOfMoney / 0.01;
+            //    amountOfMoney = amountOfMoney % 0.01;
+            //}
+            return totalMoney;
         }
 
+        public static double CheckIfValidDouble(string totalMoney)
+        {
+            var RegExPattern = "/ ^[£] ? ([1 - 9]\\d{ 0,2}(,\\d{ 3})*| 0)?(\\.\\d{ 1,2})?$/";
+            if (Regex.IsMatch(totalMoney, RegExPattern))
+            {
+                totalMoney = totalMoney.Replace("£", "");
+                return double.Parse(totalMoney);
+            }
+            else
+            {
+                return 0;
+            }
+
+        }
+        
+
     }
+
+
 }
