7a9436a2475a9375afb5766ab9c6c52af2a52528
Glen Smith <gsmith@spartaglobal.com>
2023-03-22 19:25:29 +0000

    Working code and tests minus Regex

--- TheMoneyProblemApp/TheMoneyProblemApp/Program.cs
+++ TheMoneyProblemApp/TheMoneyProblemApp/Program.cs
@@ -11,87 +11,51 @@ namespace TheMoneyProblemApp
 
         public static string ReturnDenominations(string totalMoney)
         {
-            //MULTIPLY BY 100
-            //REGEX CHECK
-            //TRY PARSE STRING TO DOUBLE
 
-            double amountOfMoney = 0;
-            int totalFivePound = 0;
-            int totalTwoPound = 0;
-            int totalOnePound = 0;
-            int totalFiftyPence = 0;
-            int totalTwentyPence = 0;
-            int totalTenPence = 0;
-            int totalFivePence = 0;
-            int totalTwoPence = 0;
-            int totalOnePence = 0;
+            if (totalMoney.Contains("£")) totalMoney = totalMoney.Replace("£", "");
+            double amountOfMoney = double.Parse(totalMoney);
 
-            if (amountOfMoney / 5 >= 1)
-            {
-                totalFivePound = (int)amountOfMoney / 5;
-                amountOfMoney = amountOfMoney % 5;
-            }
-            if (amountOfMoney / 2 > 1)
-            {
-                totalTwoPound = (int)amountOfMoney / 2;
-                amountOfMoney = amountOfMoney % 2;
-            }
-            if (amountOfMoney / 1 > 1)
-            {
-                totalOnePound = (int)amountOfMoney / 1;
-                amountOfMoney = amountOfMoney % 1;
-            }
-            //// TO BE COMPLETED
-            //if (amountOfMoney / 0.5 > 1)
-            //{
-            //    totalFiftyPence = (int)amountOfMoney / 0.5;
-            //    amountOfMoney = amountOfMoney % 0.5;
-            //}
-            //if (amountOfMoney / 0.2 > 1)
-            //{
-            //    totalTwentyPence = (int)amountOfMoney / 0.2;
-            //    amountOfMoney = amountOfMoney % 0.2;
-            //}
-            //if (amountOfMoney / 0.1 > 1)
-            //{
-            //    totalTenPence = (int)amountOfMoney / 0.1;
-            //    amountOfMoney = amountOfMoney % 0.1;
-            //}
-            //if (amountOfMoney / 0.05 > 1)
-            //{
-            //    totalFivePence = (int)amountOfMoney / 0.05;
-            //    amountOfMoney = amountOfMoney % 0.05;
-            //}
-            //if (amountOfMoney / 0.02 > 1)
-            //{
-            //    totalTwoPence = (int)amountOfMoney / 0.02;
-            //    amountOfMoney = amountOfMoney % 0.02;
-            //}
-            //if (amountOfMoney / 0.01 > 1)
-            //{
-            //    totalOnePence = (int)amountOfMoney / 0.01;
-            //    amountOfMoney = amountOfMoney % 0.01;
-            //}
-            return totalMoney;
-        }
+            if (amountOfMoney < 0) throw new ArgumentOutOfRangeException();
 
-        public static double CheckIfValidDouble(string totalMoney)
-        {
-            var RegExPattern = "/ ^[£] ? ([1 - 9]\\d{ 0,2}(,\\d{ 3})*| 0)?(\\.\\d{ 1,2})?$/";
-            if (Regex.IsMatch(totalMoney, RegExPattern))
-            {
-                totalMoney = totalMoney.Replace("£", "");
-                return double.Parse(totalMoney);
-            }
-            else
-            {
-                return 0;
-            }
+
+            int totalAmount = (int)(amountOfMoney * 100);
+
+            int totalTwoPound = totalAmount / 200;
+            totalAmount %= 200;
+
+            int totalOnePound = totalAmount / 100;
+            totalAmount %= 100;
+
+            int totalFiftyPence = totalAmount / 50;
+            totalAmount %= 50;
+
+            int totalTwentyPence = totalAmount / 20;
+            totalAmount %= 20;
+
+            int totalTenPence = totalAmount / 10;
+            totalAmount %= 10;
+
+            int totalFivePence = totalAmount / 5;
+            totalAmount %= 5;
+
+            int totalTwoPence = totalAmount / 2;
+            totalAmount %= 2;
+
+            int totalOnePence = totalAmount / 1;
+            totalAmount %= 1;
+
+            return ($"£2:{totalTwoPound} £1:{totalOnePound} £0.50:{totalFiftyPence} £0.20:{totalTwentyPence} £0.10:{totalTenPence} £0.05:{totalFivePence} £0.02:{totalTwoPence} £0.01:{totalOnePence}");
 
         }
-        
 
+        //public static double CheckIfValidDouble(string totalMoney)
+        //{
+        //    var RegExPattern = "/ ^[£] ? ([1 - 9]\\d{ 0,2}(,\\d{ 3})*| 0)?(\\.\\d{ 1,2})?$/";
+        //    if (Regex.IsMatch(totalMoney, RegExPattern))
+
+        //        totalMoney = totalMoney.Replace("£", "");
+        //        return double.Parse(totalMoney);
+        //}
     }
-
 
 }
--- TheMoneyProblemApp/TheMoneyProblemTests/MoneyProblemTest1.cs
+++ TheMoneyProblemApp/TheMoneyProblemTests/MoneyProblemTest1.cs
@@ -6,6 +6,8 @@ namespace TheMoneyProblemTests
     {
         // Valid Value Tests
         [TestCase("£5.47","£2:2 £1:1 £0.50:0 £0.20:2 £0.10:0 £0.05:1 £0.02:1 £0.01:0")]
+        [TestCase("£10.47", "£2:5 £1:0 £0.50:0 £0.20:2 £0.10:0 £0.05:1 £0.02:1 £0.01:0")]
+
         public void GivenACertainMoneyValueTheMethodReturnsExpectedDenominations(string totalMoney, string denominations)
         {
             Assert.That(Program.ReturnDenominations(totalMoney), Is.EqualTo(denominations));
@@ -16,15 +18,14 @@ namespace TheMoneyProblemTests
         [Test]
         public void GivenANegativeValue_ThrowOutOfRangeException()
         {
-            Assert.Pass();
+            Assert.That(() => Program.ReturnDenominations("-£78721.123"), Throws.TypeOf<ArgumentOutOfRangeException>());
         }
-
-        //Overflow Exceptions
 
         [Test]
-        public void GivenAOverflowOccurs_ThrowOverflowException()
+        public void GivenANegativeValueAndChar_ArgumentException()
         {
-            Assert.Pass();
+            Assert.That(() => Program.ReturnDenominations("-£78awsae721.123"), Throws.TypeOf<FormatException>());
         }
+
     }
 }
